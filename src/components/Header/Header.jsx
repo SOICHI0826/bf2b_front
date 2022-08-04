8f2acf4fa8be57c3bbb4570d644a403cc472efc6
Soichi Kitano <so-kitano@kddi.com>
2022-08-05 00:08:21 +0900

    fix:Headerコンポーネントの修正

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,40 @@
-import { css } from "@emotion/css"
-import { Text, Box, Button, Avatar } from "@chakra-ui/react"
+import { css } from "@emotion/css";
+import { Text, Box, Button, Avatar } from "@chakra-ui/react";
 
 export const Header = () => {
     return (
-          // スタイリング修正
-          <Box display='flex' justifyContent="space-between" bg='#D9D9D9' w='1486px' h='168px'>
-              <div className={styles.headerMenu}>
-                <Text ml='60px' mr='60px'>About</Text>
-                <Text mr='60px'>Works</Text>
-                <Text mr='60px'>News</Text>
-              </div>
-              <div className={styles.status}>
-                <Text>Welcome guest!</Text>
-                <Button size='lg' bg='#1D71AD' color='white' rounded='md'>Login</Button>
-                <Button size='lg' bg='#1D71AD' color='white' rounded='md'>Signup</Button>
-              </div>
-          </Box>
+        <Box display='flex' bg='#F5F5F5' w='1486px' h='168px'>
+            <div className={styles.headerMenu}>
+                <Button ml='60px' mr='60px' bg='#F5F5F5'>About</Button>
+                <Button mr='60px' bg='#F5F5F5' borderWidth='0px'>Works</Button>
+                <Button mr='60px' bg='#F5F5F5' borderWidth='0px'>News</Button>
+            </div>
+            <div className={styles.status}>
+                <Avatar size='lg' mr='45px' bg='#E2E8F0'/>
+                <Text mr='60px'>Welcome guest</Text>
+                <Button size='lg' mr='45px' bg='#1D71AD' color='white'>Login</Button>
+                <Button size='lg' bg='#1D71AD' color='white'>Signup</Button>
+            </div>
+        </Box>
     )
 }
 
 const styles = {
     headerMenu: css`
+        width: 401px;
+        height: 61px;
         display: flex;
+        flexWrap: wrap;
+        align-items: center;
+        margin-top: 40px;
+        margin-right: 450px;
     `,
     status: css`
+        width: 660px;
+        height: 64px;
         display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        margin-top: 40px;
     `
 }
