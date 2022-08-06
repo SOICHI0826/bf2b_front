e7dc3f4df99b4a4bffc4d3f93c75f41c4a702fec
Soichi Kitano <so-kitano@kddi.com>
2022-08-07 01:39:48 +0900

    feat:ログインルート追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Main } from './pages/Main';
+import { Login } from './pages/Login';
 import { WorkTest } from './pages/works/WorkTest';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={'/'} element={<Main />} />
+          <Route path={'/login'} element={<Login />} />
           <Route path={'/test'} element={<WorkTest /> } />
         </Routes>
       </BrowserRouter>
