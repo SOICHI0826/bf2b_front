719efda3027471be307a4dcfce38a9b6f05a65c8
Soichi Kitano <so-kitano@kddi.com>
2022-08-07 00:51:04 +0900

    fix：Thumコンポーネントにnavigate機能追加

--- src/components/Thumbnail/Thumbnail.jsx
+++ src/components/Thumbnail/Thumbnail.jsx
@@ -1,11 +1,16 @@
 import { css } from "@emotion/css";
 import { Text, Box, Button, Image } from "@chakra-ui/react";
+import { useNavigate } from 'react-router-dom';
 
 export const Thumbnail = (props) => {
-    const {title, thumb} = props;
+    const {title} = props;
+    const navigate = useNavigate();
+    const handleNavWork = () => {
+      navigate('/test');
+    };
     return (
         <Box pl='10px' pt='10px' pr='10px' w='414px' h='318px'>
-           <Button mb='10px' bg='#000000' w='394px' h='240px'>
+           <Button type='button' mb='10px' bg='#000000' w='394px' h='240px' onClick={handleNavWork}>
            </Button>
            <Box w='394px' h='50px'>
               <Text ml='17px'>
