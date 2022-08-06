6df8c097ed810547cbf99684d082332efe360098
Soichi Kitano <so-kitano@kddi.com>
2022-08-06 22:29:40 +0900

    fix：Footerコンポーネントスタイル修正

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,14 @@
+import { Text, Box, StylesProvider } from "@chakra-ui/react";
 import { css } from "@emotion/css";
-import { Text, Box } from "@chakra-ui/react";
 
 export const Footer = () => {
     return (
-        <Box pt='26px' pb='24px' pl='578px' pr='578px' w='1484px' h='71px'>
-          <Text fontSize='14px'>Copyright ©︎ 2022 Undex.com All Right Reserved</Text>
+        <Box w='100%' pb='15px'>
+            <Text textAlign='center' fontSize='14px'>Copyright ©︎ 2022 Undex.com All Right Reserved</Text>
         </Box>
     )
+}
+
+const styles ={
 }
 
