a240596bea472a9a3290fc6c1f520dcac2c5fa9a
Soichi Kitano <so-kitano@kddi.com>
2022-08-06 18:28:17 +0900

    feat:Footerコンポーネント作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { ChakraProvider } from "@chakra-ui/react"
 import './App.css';
 import { Header } from './components/Header/Header'
 import { Thumbnail } from './components/Thumbnail/Thumbnail'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   return (
     <ChakraProvider>
         <Thumbnail title='Metaball' thumb='../public/logo192.png'/>
+        <Footer></Footer>
     </ChakraProvider>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import { css } from "@emotion/css";
+import { Text, Box } from "@chakra-ui/react";
+
+export const Footer = () => {
+    return (
+        <Box pt='26px' pb='24px' pl='578px' pr='578px' w='1484px' h='71px'>
+          <Text fontSize='14px'>Copyright ©︎ 2022 Undex.com All Right Reserved</Text>
+        </Box>
+    )
+}
+
