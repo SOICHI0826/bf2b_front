63835be350c6ec68e98c44795734053ef399bf55
Soichi Kitano <so-kitano@kddi.com>
2022-08-13 15:50:41 +0900

    feat:signupページのルート追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Main } from './pages/Main';
 import { Login } from './pages/Login';
+import { Signup } from './pages/Signup';
 import { WorkTest } from './pages/works/WorkTest';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path={'/'} element={<Main />} />
           <Route path={'/login'} element={<Login />} />
+          <Route path={'/signup'} element={<Signup />} />
           <Route path={'/test'} element={<WorkTest /> } />
         </Routes>
       </BrowserRouter>
