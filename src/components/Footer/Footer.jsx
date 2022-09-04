083227e364ba9684f289714e1603fecd23374d87
Soichi Kitano <so-kitano@kddi.com>
2022-09-04 19:59:15 +0900

    fix；footerをページ最下部に配置

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,17 @@ import { css } from "@emotion/css";
 
 export const Footer = () => {
     return (
-        <Box w='100vw' pb='5vh'>
+        <Box className={styles.footer} w='100vw'>
             <Text textAlign='center' fontSize='14px'>Copyright ©︎ 2022 BF2B All Right Reserved</Text>
         </Box>
     )
 }
 
 const styles ={
+    //ページ最下部に固定
+    footer: css`
+        position: absolute;
+        bottom: 1vh;
+  `
 }
 
