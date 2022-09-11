ef228cf00852d95d06b2cc5f834c856ed281380b
Soichi Kitano <so-kitano@kddi.com>
2022-09-11 14:43:02 +0900

    fix：thumbnail改修

--- src/components/Thumbnail/Thumbnail.jsx
+++ src/components/Thumbnail/Thumbnail.jsx
@@ -3,21 +3,28 @@ import { Text, Box, Button, Image } from "@chakra-ui/react";
 import { useNavigate } from 'react-router-dom';
 
 export const Thumbnail = (props) => {
-    const {title, route} = props;
+    const {title, path, thumb_img, status} = props;
     const navigate = useNavigate();
     const handleNavWork = () => {
-      navigate(route);
+      navigate(path);
     };
     return (
         <Box pl='10px' pt='10px' pr='10px' w='20vw' h='28vh'>
-           <Button mb='10px' bg='#000000' w='94%' h='80%' onClick={handleNavWork}>
-            <Image src='/logo192.png' />
-           </Button>
-           <Box w='94%' h='12%'>
-              <Text>
-                { title }
-              </Text>
-           </Box>
+            {status !== 'empty' &&
+                <>
+                <Button mb='10px' bg='#223344' w='94%' h='80%' onClick={handleNavWork}>
+                    <Image src={thumb_img} w='20vw' h='20vh'/>
+                </Button>
+                <Box w='94%' h='12%'>
+                    <Text>
+                      { title }
+                    </Text>
+                </Box>
+                </>
+            }
+            {status === 'empty' &&
+                <></>
+            }
         </Box>
     )
 }
